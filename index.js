2355d94f6491445496938a095c7beef280e6fe9f
Diogo Costa <dsgmcosta@gmail.com>
2019-02-03 23:38:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n) {
+  const ranked = document.querySelectorAll("ul.ranked-list li")
+  for (let i = 0; i < ranked.length; i++) {
+    ranked[i].innerHTML = (parseInt(ranked[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild() {
+  const aux = document.querySelectorAll("#grand-node *")
+  return aux[aux.length - 1]
+}
