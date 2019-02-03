6de15aff51042a7f3d59b3e3622e01062effbbc8
Diogo Costa <dsgmcosta@gmail.com>
2019-02-03 22:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const ranked = document.querySelectorAll("ul.ranked-list li")
   for (let i = 0; i < ranked.length; i++) {
-    
+    (parseInt(ranked[i])++).toString()
   }
 }
